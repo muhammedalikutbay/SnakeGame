1dac820725c3207e4b5cb00eb289831eb34bbcd6
Muhammed Ali Kutbay <muhammedalikutbay@gmail.com>
2025-01-31 04:32:38 +0300

    Direction Added

---
+++ SnakeGame/Direction.cs
@@ -0,0 +1,46 @@
+﻿namespace SnakeGame
+{
+    internal class Direction
+    {
+        public static readonly Direction Right = new Direction(0, 1);
+        public static readonly Direction Left = new Direction(0, -1);
+        public static readonly Direction Up = new Direction(-1, 0);
+        public static readonly Direction Down = new Direction(1, 0);
+
+        public int RowOffset { get; }
+        public int ColumnOffset { get; }
+
+        private Direction(int row, int column)
+        {
+            RowOffset = row;
+            ColumnOffset = column;
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-RowOffset, -ColumnOffset);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Direction direction
+                && RowOffset == direction.RowOffset
+                && ColumnOffset == direction.ColumnOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColumnOffset);
+        }
+
+        public static bool operator ==(Direction? left, Direction? right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction? left, Direction? right)
+        {
+            return !(left == right);
+        }
+    }
+}
