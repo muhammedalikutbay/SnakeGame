6e97c31842bdd2f75c7e43e2f96ce0907bb20780
Muhammed Ali Kutbay <muhammedalikutbay@gmail.com>
2025-01-31 16:05:51 +0300

    GameLoop Method added

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -16,14 +16,61 @@ namespace SnakeGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<GridValue, ImageSource> imageSources = new Dictionary<
+            GridValue,
+            ImageSource
+        >
+        {
+            { GridValue.Empty, Images.Empty },
+            { GridValue.Snake, Images.Body },
+            { GridValue.Food, Images.Food },
+        };
+
         private readonly int rows = 15;
         private readonly int columns = 15;
         private readonly Image[,] gridImages;
+        private Game gameState;
 
         public MainWindow()
         {
             InitializeComponent();
             gridImages = SetUpGrid();
+            gameState = new Game(rows, columns);
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Draw();
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState.GameOver)
+            {
+                return;
+            }
+            switch (e.Key)
+            {
+                case Key.Up:
+                    gameState.ChangeDirection(Direction.Up);
+                    break;
+                case Key.Down:
+                    gameState.ChangeDirection(Direction.Down);
+                    break;
+                case Key.Left:
+                    gameState.ChangeDirection(Direction.Left);
+                    break;
+                case Key.Right:
+                    gameState.ChangeDirection(Direction.Right);
+                    break;
+            }
+        }
+        private async Task GameLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(100);
+                gameState.Move();
+                Draw();
+            }
         }
 
         private Image[,] SetUpGrid()
@@ -35,16 +82,29 @@ namespace SnakeGame
             {
                 for (int c = 0; c < columns; c++)
                 {
-                    Image image = new Image
-                    {
-                        Source = Images.Empty,
-                    };
+                    Image image = new Image { Source = Images.Empty };
                     GameGrid.Children.Add(image);
                     images[r, c] = image;
                 }
             }
             return images;
         }
-    }
 
+        private void Draw()
+        {
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    GridValue value = gameState.Grid[r, c];
+                    gridImages[r, c].Source = imageSources[value];
+                }
+            }
+        }
+    }
 }
