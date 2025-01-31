6e7f5f566f946ed2f9552f1fa24fd9c5ebeeff8c
Muhammed Ali Kutbay <muhammedalikutbay@gmail.com>
2025-01-31 07:09:49 +0300

    Game mechanics added

--- SnakeGame/Game.cs
+++ SnakeGame/Game.cs
@@ -1,12 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnakeGame
+﻿namespace SnakeGame
 {
-    internal class Game
+    public class Game
     {
+        public int Rows { get; }
+        public int Columns { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public Game(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            Grid = new GridValue[Rows, Columns];
+            Dir = Direction.Right;
+            Score = 0;
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+            for (int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+            }
+        }
+
+        private IEnumerable<Position> EmptyPositions()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+
+        private void AddApple()
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+            if (empty.Count == 0)
+            {
+                return;
+            }
+            Position position = empty[random.Next(empty.Count)];
+            Grid[position.Row, position.Column] = GridValue.Food;
+        }
+
+        public Position HeadPosition()
+        {
+            return snakePositions.First.Value;
+        }
+
+        public Position TailPosition()
+        {
+            return snakePositions.Last.Value;
+        }
+
+        public IEnumerable<Position> SnakePosition()
+        {
+            return snakePositions;
+        }
+
+        private void AddHead(Position position)
+        {
+            snakePositions.AddFirst(position);
+            Grid[position.Row, position.Column] = GridValue.Snake;
+        }
+
+        private void RemoveTail()
+        {
+            Position tail = snakePositions.Last.Value;
+            Grid[tail.Row, tail.Column] = GridValue.Empty;
+            snakePositions.RemoveLast();
+        }
+
+        public void ChangeDirection(Direction direction)
+        {
+            Dir = direction;
+        }
+
+        private bool OutsideGrid(Position position)
+        {
+            return position.Row < 0
+                || position.Row >= Rows
+                || position.Column < 0
+                || position.Column >= Columns;
+        }
+
+        private GridValue WillHit(Position newHeadPosition)
+        {
+            if (OutsideGrid(newHeadPosition))
+            {
+                return GridValue.Outside;
+            }
+            if (newHeadPosition == TailPosition())
+            {
+                return GridValue.Empty;
+            }
+            return Grid[newHeadPosition.Row, newHeadPosition.Column];
+        }
+
+        public void Move()
+        {
+            Position newHeadPosition = HeadPosition().Translate(Dir);
+            GridValue hit = WillHit(newHeadPosition);
+            if (hit == GridValue.Outside || hit == GridValue.Snake)
+
+            {
+                GameOver = true;
+            }
+            else if (hit == GridValue.Empty) { RemoveTail(); AddHead(newHeadPosition); }
+            else if (hit == GridValue.Food) { AddHead(newHeadPosition);Score++;AddApple(); }
+        }
     }
 }
