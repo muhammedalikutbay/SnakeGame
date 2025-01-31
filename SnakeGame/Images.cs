b5e0fb8bef9fb412f005f2aa2f9e86bd56dc99b9
Muhammed Ali Kutbay <muhammedalikutbay@gmail.com>
2025-01-31 15:08:10 +0300

    App.xaml edited

--- SnakeGame/Images.cs
+++ SnakeGame/Images.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace SnakeGame
 {
-    internal class Images
+    public static class Images
     {
+        public static readonly ImageSource Empty = LoadImage("Empty.png");
+        public static readonly ImageSource Body = LoadImage("Body.png");
+        public static readonly ImageSource DeadBody = LoadImage("DeadBody.png");
+        public static readonly ImageSource DeadHead = LoadImage("DeadHead.png");
+        public static readonly ImageSource Head = LoadImage("Head.png");
+        public static readonly ImageSource Food = LoadImage("Food.png");
+        public static readonly ImageSource Wall = LoadImage("Wall.png");
+
+        private static ImageSource LoadImage(string fileName)
+        {
+            return new BitmapImage(new Uri($"Assets/{fileName}", UriKind.Relative));
+        }
     }
 }
